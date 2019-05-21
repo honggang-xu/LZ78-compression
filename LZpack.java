ccacef0faf2ed3135614da5f3eb73604195e7767
honggang-xu <41971841+honggang-xu@users.noreply.github.com>
2019-05-21 21:21:12 +1200

    Updating comments

--- LZpack.java
+++ LZpack.java
@@ -19,94 +19,104 @@ public class LZpack
 			FileOutputStream outputFile = new FileOutputStream("packed.txt");
 			ByteArrayOutputStream writer = new ByteArrayOutputStream();
 
+			//int as the buffer to store the byte
 			int datum = 0;
+			//int used for masking
 			int mask = 0;
+			//int for counting phrase entries
 			int phraseNumValue = -1;
+			//int indicating the available bits
 			int availableBits = 32;
+			//int used for output
 			int output = 0;
 
+			//while there is input
 			while (scanner.hasNextLine())
 			{
+				//if there is an int to read
 				if (scanner.hasNextInt())
 				{
+					//read the int and store it as phrase number
 					int phraseNum = scanner.nextInt();
+					//byte for the mismatched byte
 					byte value;
-
+					//increment counter for number of phrase read
 					phraseNumValue++;
 					//bits for the phrase number
 					int phraseNumBits = Integer.toBinaryString(phraseNumValue).length();
-
+					//store it into mask
 					mask = phraseNum;
+					//shift its bits to the very left next the previous bits
 					mask <<= (availableBits - phraseNumBits);
+					//mask into the buffer
 					datum |= mask;
+					//decrement the available bits
 					availableBits -= phraseNumBits;
 
 					//if there is next byte
 					if (scanner.hasNextByte())
 					{
+						//read the byte
 						value = scanner.nextByte();
+						//store the byte into int and padded with leading zero
 						mask = value & 0xFF;
+						//shift its bits to the very left next to the previous bits
 						mask <<= (availableBits - 8);
+						//mask into the buffer
 						datum |= mask;
+						//decrement the available bits
 						availableBits -= 8;
 					}
-					/*
-					else
-					{
-						System.out.println(phraseNum);
-					}
-					*/
 
 					//while there is 8 bits to out put 
 					while ((32 - availableBits >= 8))
 					{
+						//mask the leading 8 bits
 						mask = 255;
 						mask <<= 24;
 						output = (datum & mask);
+						//8 bits padded with leading zero in int
 						output >>>= 24;
-
+						//write to the output
 						writer.write(output);
+						//left shift 8 bits in the buffer
 						datum <<= 8;
+						//increment available bit
 						availableBits += 8;
 					}
 				}
 				//else there is nothing to read next
 				else
 				{
+					//while there is bits in the buffer, output them as 8 bits at a time 
 					while (availableBits < 32)
 					{
 						int bitsRemained = (32 - availableBits) > 8 ? 8 : (32 - availableBits);
 						int bitsToShift = 32 - bitsRemained;
-
 						mask = -1;
 						mask <<= 24;
 						output = (datum & mask);
 						output >>>= 24;
-
 						writer.write(output);
 						datum <<= 8;
 						availableBits += 8;
 					}
-					
 					scanner.nextLine();
 				}
 			}
-
+			//while there is bits in the buffer, output them as 8 bits at a time
 			while (availableBits < 32)
 			{
 				int bitsRemained = (32 - availableBits) > 8 ? 8 : (32 - availableBits);
 				int bitsToShift = 32 - bitsRemained;
-
 				mask = -1;
 				mask <<= 24;
 				output = (datum & mask);
 				output >>>= 24;
-
 				writer.write(output);
 				datum <<= 8;
 				availableBits += 8;
 			}
-
 			writer.writeTo(outputFile);
 			writer.close();
 			System.out.println("process finished");
--- LZunpack.java
+++ LZunpack.java
@@ -14,94 +14,117 @@ public class LZunpack
 			FileWriter file = new FileWriter("unpacked.txt");
 			BufferedWriter writer = new BufferedWriter(file);
 
+			//int as the buffer to store the byte
 			int datum = 0;
+			//int used for masking
 			int mask = 0;
+			//int for counting phrase entries
 			int phraseNumValue = -1;
+			//int indicating the available bits
 			int availableBits = 32;
+			//index value
 			int index = 0;
+			//mismatched byte as int
 			int phrase = 0;
-
+			//int for reading input
 			int i;
+			//flag indicating whether there is input
 			boolean flag = true;
-			//int entryCount = 0;
 			
+			//fill the buffer with 32 bits if there is 32 bits in input
 			while (availableBits != 0)
 			{
-				//System.out.println("availableBits" + availableBits);
-				
+				//if there is input, read one byte
 				if ((i = inputs.read()) != -1)
 				{
-					//System.out.println("has next");
+					
 					int nextByte = i;
-					//System.out.println("next byte " + nextByte);
+					//shift the 8 bits to the very left next to the previous bits
 					nextByte <<= (availableBits - 8);
+					//or in buffer
 					datum |= nextByte;
+					//decrement the available bits
 					availableBits -= 8;
-					//System.out.println(entryCount);
 				}
+				//else there is no input
 				else
 				{
+					//while there is 8 bits to output
 					while ((32 - availableBits) > 8)
 					{
+						//increment counter for number of phrase read
 						phraseNumValue++;
+						//bits for the phrase number
 						int phraseNumBits = Integer.toBinaryString(phraseNumValue).length();
+
+						//set up the mask 
 						mask = -1;
 						mask <<= (32 - phraseNumBits);
+						//get the phrase number value
 						index = datum & mask;
+						//shift the buffer
 						datum <<= phraseNumBits;
-
+						//right shift the phrase number padded with leading 0 in an int
 						index >>>= 32 - phraseNumBits;
+						//increment the available bits
 						availableBits += phraseNumBits;
 
+						//set up the mask
 						mask = 255;
 						mask <<= 24;
+						//get the mismatched byte as an int
 						phrase = datum & mask;
+						//shift the buffer
 						datum <<= 8;
-
+						//right shift the mismatched byte padded with leading 0 in an int
 						phrase >>>= 24;
+						//increment the available bits
 						availableBits += 8;
-
+						//write the phrase number and mismatched byte to the output
 						writer.write(index + " " + (byte)phrase);
 						writer.newLine();
-						
 					}
 					availableBits = 0;
 				}
 			}
-			
-			
-			
+			//while there is input
 			while (flag)
 			{
-				
+				//increment counter for number of phrase read
 				phraseNumValue++;
+				//bits for the phrase number
 				int phraseNumBits = Integer.toBinaryString(phraseNumValue).length();
-				
+				//set up the mask
 				mask = -1;
 				mask <<= (32 - phraseNumBits);
+				//get the phrase number value
 				index = datum & mask;
+				//shift the buffer
 				datum <<= phraseNumBits;
-
+				//right shift the phrase number padded with leading 0 in an int
 				index >>>= 32 - phraseNumBits;
+				//increment the available bits
 				availableBits += phraseNumBits;
 				
-
+				//set up the mask
 				mask = 255;
 				mask <<= 24;
+				//get the mismatched byte as an int
 				phrase = datum & mask;
+				//shift the buffer
 				datum <<= 8;
-
+				//right shift the mismatched byte padded with leading 0 in an int
 				phrase >>>= 24;
+				//increment the available bits
 				availableBits += 8;
-				
-
+				//write the phrase number and mismatched byte to the output
 				writer.write(index + " " + (byte)phrase);
-				
 				writer.newLine();
 
 				//while there is next input and has 8 bits available
 				while (availableBits >= 8)
 				{
+					//if there is input, read one byte and stor it into buffer
 					if ((i = inputs.read()) != -1)
 					{
 						int nextByte = i;
@@ -113,32 +136,31 @@ public class LZunpack
 					else
 					{
 						flag = false;
+						//while there is 8 bits to output in the buffer, output them
 						while ((32 - availableBits) > 8)
 						{
-							
 							phraseNumValue++;
 							phraseNumBits = Integer.toBinaryString(phraseNumValue).length();
 							mask = -1;
 							mask <<= (32 - phraseNumBits);
 							index = datum & mask;
 							datum <<= phraseNumBits;
-
 							index >>>= 32 - phraseNumBits;
 							availableBits += phraseNumBits;
-
 							mask = 255;
 							mask <<= 24;
 							phrase = datum & mask;
 							datum <<= 8;
-
 							phrase >>>= 24;
 							availableBits += 8;
 
+							//if there is mismatched byte
 							if (phrase != 0)
 							{
 								writer.write(index + " " + (byte)phrase);
 								writer.newLine();
 							}
+							//else there is no mismatched byte, just output the phrase number
 							else
 							{
 								writer.write(index + "");
